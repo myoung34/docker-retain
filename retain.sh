5dec171925e933f734242e48e125d991727b66d9
Marcus Young <myoung34@my.apsu.edu>
2020-08-14 06:14:33 -0500

    Unleash

--- retain.sh
+++ retain.sh
@@ -3,7 +3,7 @@ user=$1
 
 [[ ! -d tmp ]] && mkdir tmp
 
-for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=100" | jq -r .results[0].name); do 
+for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=100" | jq -r .results[].name); do 
   tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags?page_size=100" | jq -r '.results[] | .name')
   for tag in ${tags}; do
     variant=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
