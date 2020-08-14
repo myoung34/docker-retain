f6b065862f7c58b8d775491b01dd148f7ffa9ecd
Marcus Young <myoung34@my.apsu.edu>
2020-08-14 05:53:30 -0500

    Lets do this thing

--- retain.sh
+++ retain.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 user=$1
+
+mkdir tmp
+
 for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=100" | jq -r .results[0].name); do 
   tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags?page_size=100" | jq -r '.results[] | .name')
   for tag in ${tags}; do
     variant=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
-    tempfile1=$(mktemp)
-    echo "FROM ${repo}:${tag}" >"${tempfile1}"
-    echo docker buildx build -t "${user}/${repo}:${tag}" --output "type=image,push=false" -f "${tempfile1}" --platform "${variant}" .
+    tempdir1="tmp/${variant}"
+    mkdir -p "${tempdir1}"
+    tempfile1="${tempdir1}/${repo}_${tag}"
+    echo "FROM ${user}/${repo}:${tag}" >"${tempfile1}"
+    docker buildx build -t "$(uuidgen)" --output "type=image,push=false" -f "${tempfile1}" --platform "${variant}" .
     no_variant=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}" | jq -r '.images[] | select (.variant == null) |  (.os + "/" + .architecture)')
     for item in $no_variant; do
-      tempfile2=$(mktemp)
-      echo "FROM ${repo}:${tag}" >"${tempfile2}"
-      echo docker buildx build -t "${user}/${repo}:${tag}" --output "type=image,push=false" -f "${tempfile2}" --platform "${item}" . -f "${tempfile2}"
+      tempdir2="tmp/${variant}"
+      mkdir -p "${tempdir2}"
+      tempfile2="${tempdir2}/${repo}_${tag}"
+      echo "FROM ${user}/${repo}:${tag}" >"${tempfile2}"
+      docker buildx build -t "$(uuidgen)" --output "type=image,push=false" -f "${tempfile2}" --platform "${item}" . -f "${tempfile2}"
     done
   done
 done
