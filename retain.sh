b97b1af74b0e19fa136e6a9cc78378a7b9e87243
Marcus Young <myoung34@my.apsu.edu>
2020-08-14 06:45:57 -0500

    Dont crawl tags twice

--- retain.sh
+++ retain.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 user=$1
+page_size=100
 
 [[ ! -d tmp ]] && mkdir tmp
 
-for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=100" | jq -r .results[].name); do 
-  tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags?page_size=100" | jq -r '.results[] | .name')
+for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=${page_size}" | jq -r .results[].name); do 
+  tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags?page_size=${page_size}" | jq -r '.results[] | .name')
   for tag in ${tags}; do
-    variant=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
-    no_variant=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}" | jq -r '.images[] | select (.variant == null) |  (.os + "/" + .architecture)')
+    tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}?page_size=${page_size}")
+    variant=$(echo "${tags}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
+    no_variant=$(echo "${tags}" | jq -r '.images[] | select (.variant == null) |  (.os + "/" + .architecture)')
     platforms=$(echo "${variant},${no_variant}" | tr '\n' ',' | sed 's/,$//g')
     tempdir="tmp/${variant}"
     mkdir -p "${tempdir}"
