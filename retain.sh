fd23149d01e2729c88f3ca9711c5100293ea913a
Marcus Young <myoung34@my.apsu.edu>
2020-10-22 20:47:30 -0500

    Syntax fixes

--- retain.sh
+++ retain.sh
@@ -8,14 +8,13 @@ page_size=100
 for repo in $(curl -sL "https://hub.docker.com/v2/repositories/${user}?page_size=${page_size}" | jq -r ".results[] | select(.name | startswith(\"$starting_char\")) | .name"); do 
   tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags?page_size=${page_size}" | jq -r '.results[] | .name')
   for tag in ${tags}; do
-    tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}?page_size=${page_size}")
-    variant=$(echo "${tags}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
-    no_variant=$(echo "${tags}" | jq -r '.images[] | select (.variant == null) |  (.os + "/" + .architecture)')
-    platforms=$(echo "${variant},${no_variant}" | tr '\n' ',' | sed 's/,$//g')
-    tempdir="tmp/${variant}"
-    mkdir -p "${tempdir}"
-    tempfile="${tempdir}/${repo}_${tag}"
+    _tags=$(curl -sL "https://hub.docker.com/v2/repositories/${user}/${repo}/tags/${tag}?page_size=${page_size}")
+    variant=$(echo "${_tags}" | jq -r '.images[] | select (.variant != null) |  (.os + "/" + .architecture + "/" + .variant)')
+    no_variant=$(echo "${_tags}" | jq -r '.images[] | select (.variant == null) |  (.os + "/" + .architecture)')
+    platforms=$(echo "${variant},${no_variant}" | tr '\n' ',' | sed 's/^,//g'  | sed 's/,$//g' | tr -d '\n')
+    tempfile=$(uuidgen)
     echo "FROM ${user}/${repo}:${tag}" >"${tempfile}"
-    docker buildx build -t "$(uuidgen)" --output "type=image,push=false" -f "${tempfile}" --platform "${platforms}" . || :
+    echo "Pulling ${user}/${repo}:${tag} with platforms ${platforms}"
+    docker buildx build -f ${tempfile} -t $user/$repo:$tag --output "type=image,push=false" --platform ${platforms} . || :
   done
 done
